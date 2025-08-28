862a99a48abd2001fbfe01d4a7445a9b33f278f7
Leo Park <leoppark94@gmail.com>
2025-08-28 16:01:47 +0100

    Move the provider block to root module

--- applications/gitlab/provider.tf
+++ applications/gitlab/provider.tf
@@ -2,11 +2,7 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "3.6.2"
+      version = ">= 3.6.2"
     }
   }
-}
-
-provider "docker" {
-  host = "unix:///var/run/docker.sock"
 }
